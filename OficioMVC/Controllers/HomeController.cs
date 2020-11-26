8db435b6b58dfe2a68fce1330346f61846b8a30d
Wellington Andrade <weg.andrade@gmail.com>
2020-11-26 18:07:58 -0300

    Limpando o controller home

--- OficioMVC/Controllers/HomeController.cs
+++ OficioMVC/Controllers/HomeController.cs
@@ -74,40 +74,13 @@ namespace OficioMVC.Controllers
             //HttpContext.Session.SetObjectAsJson("User", null);
             return RedirectToAction("Login");
         }
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-
-
-
-
-        }
-
-        public IActionResult Contact()
-        {
-
-
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-
-
-
-
-        }
-
+       
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
         public IActionResult AcessoNegado()
         {
             return View();
